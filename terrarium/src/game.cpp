ea0c3aa74b3691b9f89450f8cacf627e25e07149
Carlos Faruolo <5carlosfelipe5@gmail.com>
2015-05-17 23:21:31 -0300

    Moving methods inside declaration of GameStuff. Added GameStuff destructor. Leaks persists.

--- terrarium/src/game.cpp
+++ terrarium/src/game.cpp
@@ -12,6 +12,7 @@
 #include "entity.hpp"
 #include "map.hpp"
 #include "menu.hpp"
+#include "util.hpp"
 
 using Physics::World;
 using Physics::Vector;
@@ -40,281 +41,299 @@ struct GameStuff
 	bool running, jumping, inGameMenuShowing;
 	bool isKeyUpPressed, isKeyDownPressed, isKeyRightPressed, isKeyLeftPressed;
 
-	GameStuff(const string& map_path);
-
-	void start();
-	void handleInput();
-	void drawScene();
-	void drawDebug();
-};
-
-GameStuff::GameStuff(const string& map_path)
-{
-	visibleArea = Rect(0,0,640,480);
-
-	//loading font
-	font = new Engine::Font("resources/liberation.ttf", 14);
-
-	//loading ingame menu
-	inGameMenu = new Menu(Rect(200, 200, 200, 64), font, Color::ORANGE, true);
-	inGameMenu->addEntry("Resume");
-	inGameMenu->addEntry("Save and exit");
-	inGameMenu->addEntry("Exit without saving");
-
-	//loading event queue
-	eventQueue = new Engine::EventQueue();
-
-	//setting flags
-	running = true;
-	jumping = false;
-	inGameMenuShowing = false;
-	isKeyUpPressed = false;
-	isKeyDownPressed = false;
-	isKeyRightPressed = false;
-	isKeyLeftPressed = false;
-
-	//loading world
-	Vector gravity(0.0f, 10.0f);
-	world = new World(gravity);
-
-	//loading map
-	game_map = Map::loadMapFromFile(map_path, world);
-	game_map->visibleArea = &visibleArea;
-
-	//loading player graphics
-	Image* player_img = new Image("resources/pijamaman-1.png");
-	AnimationSet* anim = new AnimationSet(player_img);
-	anim->add("still-left", 56, 84, 1, 1);
-	anim->add("still-right", 56, 84, 1, 1);
-	anim->add("walk-left", 56, 84, 0.1, 4);
-	anim->add("walk-right", 56, 84, 0.1, 4);
-	anim->setCurrent("still-right");
-
-	//loading player physics
-	Body* b = new Body(1,1,Math::convertToMeters(16), Math::convertToMeters(80));
-	player = new Entity(anim, b, &visibleArea);
-	player->body->setDynamic();
-	world->addBody(player->body);
-	player->body->setFixedRotation();
-
-	//loading dirt tileset
-	tileset_dirt = new Image("resources/tileset-dirt.png");
-	cout << "map size (in pixels): " << game_map->computeDimensions().w << "x" << game_map->computeDimensions().h << endl;
-}
-
-void GameStuff::start()
-{
-	while(running)
+	GameStuff(const string& map_path)
 	{
-		//visible area quick n' dirt fix
-		{
-			visibleArea.x = Math::convertToPixels(player->body->getX()) - visibleArea.w/2.0;
-			visibleArea.y = Math::convertToPixels(player->body->getY()) - visibleArea.h/2.0;
-			if(visibleArea.x + visibleArea.w > game_map->computeDimensions().w)
-				visibleArea.x = game_map->computeDimensions().w - visibleArea.w;
-			if(visibleArea.x < 0)
-				visibleArea.x = 0;
-			if(visibleArea.y + visibleArea.h > game_map->computeDimensions().h)
-				visibleArea.y = game_map->computeDimensions().h - visibleArea.h;
-			if(visibleArea.y < 0)
-				visibleArea.y = 0;
-		}
+		visibleArea = Rect(0,0,640,480);
 
-		handleInput();
-		drawScene();
+		//loading font
+		font = new Engine::Font("resources/liberation.ttf", 14);
+
+		//loading ingame menu
+		inGameMenu = new Menu(Rect(200, 200, 200, 64), font, Color::ORANGE, true);
+		inGameMenu->addEntry("Resume");
+		inGameMenu->addEntry("Save and exit");
+		inGameMenu->addEntry("Exit without saving");
+
+		//loading event queue
+		eventQueue = new Engine::EventQueue();
+
+		//setting flags
+		running = true;
+		jumping = false;
+		inGameMenuShowing = false;
+		isKeyUpPressed = false;
+		isKeyDownPressed = false;
+		isKeyRightPressed = false;
+		isKeyLeftPressed = false;
+
+		//loading world
+		Vector gravity(0.0f, 10.0f);
+		world = new World(gravity);
+
+		//loading map
+		game_map = Map::loadMapFromFile(map_path, world);
+		game_map->visibleArea = &visibleArea;
+
+		//loading player graphics
+		Image* player_img = new Image("resources/pijamaman-1.png");
+		AnimationSet* anim = new AnimationSet(player_img);
+		anim->add("still-left", 56, 84, 1, 1);
+		anim->add("still-right", 56, 84, 1, 1);
+		anim->add("walk-left", 56, 84, 0.1, 4);
+		anim->add("walk-right", 56, 84, 0.1, 4);
+		anim->setCurrent("still-right");
+
+		//loading player physics
+		Body* b = new Body(1,1,Math::convertToMeters(16), Math::convertToMeters(80));
+		player = new Entity(anim, b, &visibleArea);
+		player->body->setDynamic();
+		world->addBody(player->body);
+		player->body->setFixedRotation();
+
+		//loading dirt tileset
+		tileset_dirt = new Image("resources/tileset-dirt.png");
+		cout << "map size (in pixels): " << game_map->computeDimensions().w << "x" << game_map->computeDimensions().h << endl;
 	}
 
-	delete player;
-}
-
-void GameStuff::handleInput()
-{
-
-	if(not inGameMenuShowing and isKeyUpPressed and !jumping) {
-		Vector v = player->body->getVelocity();
-		if(v.y >= -2.0f) {
-			player->body->applyImpulse(Vector(0.0f, -0.05f), Vector(player->body->getX(), player->body->getY()));
-			jumping = true;
-		}
-	}
-	if(not inGameMenuShowing and isKeyDownPressed) {
-		Vector v = player->body->getVelocity();
-		if(v.y <= 2.0f)
-			player->body->applyForceToCenter(Vector(0.0f, 0.2f));
-	}
-	if(not inGameMenuShowing and isKeyRightPressed) {
-		player->animation->setCurrent("walk-right");
-		Vector v = player->body->getVelocity();
-		if(v.x <= 2.0f)
-			player->body->applyForceToCenter(Vector(0.2f, 0.0f));
-	}
-	if(not inGameMenuShowing and isKeyLeftPressed) {
-		player->animation->setCurrent("walk-left");
-		Vector v = player->body->getVelocity();
-		if(v.x >= -2.0f)
-			player->body->applyForceToCenter(Vector(-0.2f, 0.0f));
-	}
-
-	while(! eventQueue->isEmpty() )
+	~GameStuff()
 	{
-		Engine::Event* ev = eventQueue->waitForEvent();
 
-		if(ev->getEventType() == Engine::Event::Type::DISPLAY_CLOSURE)
+		foreach(Entity*, e, vector<Entity*>, entities)
 		{
-			running=false;
+			delete e;
 		}
-		else if(ev->getEventType() == Engine::Event::Type::KEY_RELEASE)
+
+		foreach(Image*, img, vector<Image*>, images)
 		{
-			switch(ev->getEventKeyCode())
+			delete img;
+		}
+
+		foreach(Block*, b, vector<Block*>, blocks)
+		{
+			delete b;
+		}
+
+		delete player;
+		delete game_map;
+		delete tileset_dirt;
+		delete font;
+		delete inGameMenu;
+		delete eventQueue;
+		delete world;
+	}
+
+	void start()
+	{
+		while(running)
+		{
+			//visible area quick n' dirt fix
 			{
-			case Engine::Event::Key::ARROW_UP:
-				isKeyUpPressed = false;
-				jumping = false;
-				break;
-			case Engine::Event::Key::ARROW_DOWN:
-				isKeyDownPressed = false;
-				break;
-			case Engine::Event::Key::ARROW_RIGHT:
-				player->animation->setCurrent("still-right");
-				isKeyRightPressed = false;
-				break;
-			case Engine::Event::Key::ARROW_LEFT:
-				player->animation->setCurrent("still-left");
-				isKeyLeftPressed = false;
-				break;
-			default:
-				break;
+				visibleArea.x = Math::convertToPixels(player->body->getX()) - visibleArea.w/2.0;
+				visibleArea.y = Math::convertToPixels(player->body->getY()) - visibleArea.h/2.0;
+				if(visibleArea.x + visibleArea.w > game_map->computeDimensions().w)
+					visibleArea.x = game_map->computeDimensions().w - visibleArea.w;
+				if(visibleArea.x < 0)
+					visibleArea.x = 0;
+				if(visibleArea.y + visibleArea.h > game_map->computeDimensions().h)
+					visibleArea.y = game_map->computeDimensions().h - visibleArea.h;
+				if(visibleArea.y < 0)
+					visibleArea.y = 0;
+			}
+
+			handleInput();
+			drawScene();
+		}
+	}
+
+	void handleInput()
+	{
+
+		if(not inGameMenuShowing and isKeyUpPressed and !jumping) {
+			Vector v = player->body->getVelocity();
+			if(v.y >= -2.0f) {
+				player->body->applyImpulse(Vector(0.0f, -0.05f), Vector(player->body->getX(), player->body->getY()));
+				jumping = true;
 			}
 		}
-		else if(ev->getEventType() == Engine::Event::Type::KEY_PRESS)
+		if(not inGameMenuShowing and isKeyDownPressed) {
+			Vector v = player->body->getVelocity();
+			if(v.y <= 2.0f)
+				player->body->applyForceToCenter(Vector(0.0f, 0.2f));
+		}
+		if(not inGameMenuShowing and isKeyRightPressed) {
+			player->animation->setCurrent("walk-right");
+			Vector v = player->body->getVelocity();
+			if(v.x <= 2.0f)
+				player->body->applyForceToCenter(Vector(0.2f, 0.0f));
+		}
+		if(not inGameMenuShowing and isKeyLeftPressed) {
+			player->animation->setCurrent("walk-left");
+			Vector v = player->body->getVelocity();
+			if(v.x >= -2.0f)
+				player->body->applyForceToCenter(Vector(-0.2f, 0.0f));
+		}
+
+		while(! eventQueue->isEmpty() )
 		{
-			switch(ev->getEventKeyCode())
+			Engine::Event* ev = eventQueue->waitForEvent();
+
+			if(ev->getEventType() == Engine::Event::Type::DISPLAY_CLOSURE)
 			{
-			case Engine::Event::Key::ARROW_UP:
-				isKeyUpPressed = true;
-				if(inGameMenuShowing)
-					--*inGameMenu;
-				break;
-			case Engine::Event::Key::ARROW_DOWN:
-				isKeyDownPressed = true;
-				if(inGameMenuShowing)
-					++*inGameMenu;
-				break;
-			case Engine::Event::Key::ARROW_RIGHT:
-				isKeyRightPressed = true;
-				break;
-			case Engine::Event::Key::ARROW_LEFT:
-				isKeyLeftPressed = true;
-				break;
-			case Engine::Event::Key::ESCAPE:
-				if(inGameMenuShowing)
-					inGameMenuShowing=false;
-				else
-					inGameMenuShowing=true;
-				break;
-			case Engine::Event::Key::ENTER:
-				if(inGameMenuShowing)
+				running=false;
+			}
+			else if(ev->getEventType() == Engine::Event::Type::KEY_RELEASE)
+			{
+				switch(ev->getEventKeyCode())
 				{
-					switch(inGameMenu->selectedIndex)
-					{
-						case 0:
-							inGameMenuShowing=false;
-							break;
-						case 1:
-							Map::saveRawMapToFile(string("resources/maps/saved_map.txt"), game_map);
-							running=false;
-							break;
-						case 2:
-							running=false;
-							break;
-					}
+				case Engine::Event::Key::ARROW_UP:
+					isKeyUpPressed = false;
+					jumping = false;
+					break;
+				case Engine::Event::Key::ARROW_DOWN:
+					isKeyDownPressed = false;
+					break;
+				case Engine::Event::Key::ARROW_RIGHT:
+					player->animation->setCurrent("still-right");
+					isKeyRightPressed = false;
+					break;
+				case Engine::Event::Key::ARROW_LEFT:
+					player->animation->setCurrent("still-left");
+					isKeyLeftPressed = false;
+					break;
+				default:
+					break;
 				}
-				break;
-			default:
-				break;
+			}
+			else if(ev->getEventType() == Engine::Event::Type::KEY_PRESS)
+			{
+				switch(ev->getEventKeyCode())
+				{
+				case Engine::Event::Key::ARROW_UP:
+					isKeyUpPressed = true;
+					if(inGameMenuShowing)
+						--*inGameMenu;
+					break;
+				case Engine::Event::Key::ARROW_DOWN:
+					isKeyDownPressed = true;
+					if(inGameMenuShowing)
+						++*inGameMenu;
+					break;
+				case Engine::Event::Key::ARROW_RIGHT:
+					isKeyRightPressed = true;
+					break;
+				case Engine::Event::Key::ARROW_LEFT:
+					isKeyLeftPressed = true;
+					break;
+				case Engine::Event::Key::ESCAPE:
+					if(inGameMenuShowing)
+						inGameMenuShowing=false;
+					else
+						inGameMenuShowing=true;
+					break;
+				case Engine::Event::Key::ENTER:
+					if(inGameMenuShowing)
+					{
+						switch(inGameMenu->selectedIndex)
+						{
+							case 0:
+								inGameMenuShowing=false;
+								break;
+							case 1:
+								Map::saveRawMapToFile(string("resources/maps/saved_map.txt"), game_map);
+								running=false;
+								break;
+							case 2:
+								running=false;
+								break;
+						}
+					}
+					break;
+				default:
+					break;
+				}
+			}
+			else if(ev->getEventType() == Engine::Event::Type::MOUSE_BUTTON_PRESS)
+			{
+
+				if(ev->getEventMouseButton() == Engine::Event::MouseButton::RIGHT)
+				{
+					unsigned int mx = (visibleArea.x + ev->getEventMouseX())/BLOCK_SIZE;
+					unsigned int my = (visibleArea.y + ev->getEventMouseY())/BLOCK_SIZE;
+
+					if(mx < game_map->grid.capacity() && my < game_map->grid[0].capacity()) // in case you click outside the map
+						if (game_map->grid[mx][my] == NULL)
+						{
+							game_map->grid[mx][my] = new Block(tileset_dirt, mx, my, 1);
+							world->addBody(game_map->grid[mx][my]->body);
+							game_map->retile(game_map->grid[mx][my]);
+						}
+
+				}
+				else if (ev->getEventMouseButton() == Engine::Event::MouseButton::LEFT)
+				{
+					unsigned int mx = (visibleArea.x + ev->getEventMouseX())/BLOCK_SIZE;
+					unsigned int my = (visibleArea.y + ev->getEventMouseY())/BLOCK_SIZE;
+
+					if(mx < 0) mx = 0; // safety
+					if(my < 0) my = 0; // safety
+					if(mx < game_map->grid.capacity() && my < game_map->grid[0].capacity()) // in case you click outside the map
+						if (game_map->grid[mx][my] != NULL)
+						{
+							world->destroyBody(game_map->grid[mx][my]->body);
+							delete game_map->grid[mx][my];
+							game_map->grid[mx][my] = NULL;
+							game_map->retileNeighbourhood(mx, my);
+						}
+				}
 			}
 		}
-		else if(ev->getEventType() == Engine::Event::Type::MOUSE_BUTTON_PRESS)
-		{
 
-			if(ev->getEventMouseButton() == Engine::Event::MouseButton::RIGHT)
-			{
-				unsigned int mx = (visibleArea.x + ev->getEventMouseX())/BLOCK_SIZE;
-				unsigned int my = (visibleArea.y + ev->getEventMouseY())/BLOCK_SIZE;
+	}
 
-				if(mx < game_map->grid.capacity() && my < game_map->grid[0].capacity()) // in case you click outside the map
-					if (game_map->grid[mx][my] == NULL)
-					{
-						game_map->grid[mx][my] = new Block(tileset_dirt, mx, my, 1);
-						world->addBody(game_map->grid[mx][my]->body);
-						game_map->retile(game_map->grid[mx][my]);
-					}
+	void drawScene()
+	{
+		Engine::display->clear();
+		world->step((1.0f / 60.0f), 6, 2);
+		/* needs to draw HUD */
 
-			}
-			else if (ev->getEventMouseButton() == Engine::Event::MouseButton::LEFT)
-			{
-				unsigned int mx = (visibleArea.x + ev->getEventMouseX())/BLOCK_SIZE;
-				unsigned int my = (visibleArea.y + ev->getEventMouseY())/BLOCK_SIZE;
+		game_map->draw_bg_player();
 
-				if(mx < 0) mx = 0; // safety
-				if(my < 0) my = 0; // safety
-				if(mx < game_map->grid.capacity() && my < game_map->grid[0].capacity()) // in case you click outside the map
-					if (game_map->grid[mx][my] != NULL)
-					{
-						world->destroyBody(game_map->grid[mx][my]->body);
-						delete game_map->grid[mx][my];
-						game_map->grid[mx][my] = NULL;
-						game_map->retileNeighbourhood(mx, my);
-					}
-			}
+		player->draw(); //draw player
+
+		game_map->draw_fg_player();
+
+		/* drawing others entities */
+		for(vector<Entity*>::iterator it = entities.begin() ; it != entities.end(); ++it){
+			if (*it != NULL) (*it)->draw();
 		}
+
+		/* later should be a character class */
+
+		//DEBUG!!!
+		drawDebug();
+
+		if(inGameMenuShowing)
+			inGameMenu->draw();
+
+
+		Engine::display->refresh();
 	}
 
-}
 
-void GameStuff::drawScene()
-{
-	Engine::display->clear();
-	world->step((1.0f / 60.0f), 6, 2);
-	/* needs to draw HUD */
+	void drawDebug()
+	{
+		font->draw_text("## DEBUG BUILD ##", 245, 0, Engine::Color::RED);
 
-	game_map->draw_bg_player();
+		font->draw_text("POSITION", 0, 14, Engine::Color::WHITE);
 
-	player->draw(); //draw player
+		font->draw_text(string("x: ")+player->body->getX()+" y:"+player->body->getY(), 0, 28, Engine::Color::WHITE);
 
-	game_map->draw_fg_player();
+		font->draw_text("SPEED", 0, 42, Engine::Color::WHITE);
 
-	/* drawing others entities */
-	for(vector<Entity*>::iterator it = entities.begin() ; it != entities.end(); ++it){
-		if (*it != NULL) (*it)->draw();
+		font->draw_text(string("x: ")+player->body->getVelocity().x+" y: "+player->body->getVelocity().y, 0, 56, Engine::Color::WHITE);
+
 	}
-
-	/* later should be a character class */
-
-	//DEBUG!!!
-	drawDebug();
-
-	if(inGameMenuShowing)
-		inGameMenu->draw();
-
-
-	Engine::display->refresh();
-}
-
-void GameStuff::drawDebug()
-{
-	font->draw_text("## DEBUG BUILD ##", 245, 0, Engine::Color::RED);
-
-	font->draw_text("POSITION", 0, 14, Engine::Color::WHITE);
-
-	font->draw_text(string("x: ")+player->body->getX()+" y:"+player->body->getY(), 0, 28, Engine::Color::WHITE);
-
-	font->draw_text("SPEED", 0, 42, Engine::Color::WHITE);
-
-	font->draw_text(string("x: ")+player->body->getVelocity().x+" y: "+player->body->getVelocity().y, 0, 56, Engine::Color::WHITE);
-
-}
-
+};
 
 //===========================================================================================
 GameStuff* stuff;

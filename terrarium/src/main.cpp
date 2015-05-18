9aeb925ce3c0a8272ea3b7a22e1aef226fdcbb15
Carlos Faruolo <5carlosfelipe5@gmail.com>
2015-05-17 22:34:15 -0300

    Added version.

--- terrarium/src/main.cpp
+++ terrarium/src/main.cpp
@@ -14,12 +14,14 @@
 using std::cout;
 using std::endl;
 
+string VERSION = "0.1.0-alpha";
+
 int main(int argc, char** argv)
 {
 	try
 	{
 		Engine::initialize();
-		Engine::display = new Engine::Display(640, 480, "Project Terrarium");
+		Engine::display = new Engine::Display(640, 480, "Project Terrarium (v"+VERSION+")");
 
 //		cout << "Lets test the directory listing method" << endl;
 //		list<String> dirs = Engine::getFilenamesWithinDirectory("./resources/maps");

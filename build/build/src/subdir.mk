de042186eadc6389cc499654860eabd5538fc458
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-11-10 15:45:22 -0200

    Added prototype makefile for fgeal-based terrarium.

---
+++ build/build/src/subdir.mk
@@ -0,0 +1,21 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../build/src/animation.o \
+../build/src/block.o \
+../build/src/entity.o \
+../build/src/file_parser.o \
+../build/src/game.o \
+../build/src/main.o \
+../build/src/main_menu.o \
+../build/src/map.o \
+../build/src/menu.o \
+../build/src/physics.o \
+../build/src/util.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+

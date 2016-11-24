2dd5c0bd1ef483d3c304fbd5aa09690eba0aed1d
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-11-24 14:49:33 -0200

    Removed current makefiles. Updated eclipse makefile patcher.

--- devpatch_makefiles.sh
+++ devpatch_makefiles.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 echo "### Preparing makefiles for prime time ###"
 echo "- Erasing content of build folder (except configure.sh)..."
-mv build/configure.sh .
-rm build/* -rf
-mv configure.sh build/
-echo "- Performing make clean in linux-gcc-release folder..."
-cd linux-gcc-release
+mv build/$1/configure.sh .
+rm build/$1/* -rf
+mv configure.sh build/$1/
+echo "- Performing make clean in $1 folder..."
+cd $1
 make clean
 cd ..
-echo "- Copying makefiles from linux-gcc-release folder to build folder..."
-cp linux-gcc-release/* -r build/
+echo "- Copying makefiles from $1 folder to build folder..."
+cp $1/* -r build/$1/
 echo "- Replacing current folder with generic BUILD_PATH token..."
-./touch_makefiles.sh ./build/ ${PWD} BUILD_PATH
-echo "- Replacing current fgeal include folder with generic FGEAL_INCLUDE_PATH token..."
-./touch_makefiles.sh ./build/ $1 FGEAL_INCLUDE_PATH
-echo "- Replacing current fgeal include folder with generic FGEAL_LIBS_PATH token..."
-find ./build/ -name makefile -type f -exec echo "Patched " {} \;
-find ./build/ -name makefile -type f -exec sed -i "s,$2,FGEAL_LIBS_PATH,g" {} \;
+./touch_makefiles.sh ./build/$1/ ${PWD} BUILD_PATH
 
 echo "Done."

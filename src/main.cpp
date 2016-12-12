41c38cf69d2a57a3e879fe0fc3a28d30c5a35450
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-12-12 18:22:46 -0200

    Using TerrariumGame class in main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -9,35 +9,44 @@
 #include <stdexcept>
 
 #include "fgeal/fgeal.hpp"
-#include "file_parser.hpp"
-#include "main_menu.hpp"
+using fgeal::Image;
+
+#include "terrarium_game.hpp"
 
 #include <iostream>
 using std::cout;
 using std::endl;
 
-string VERSION = "0.1.3";
+#include <string>
+using std::string;
+
+#include <vector>
+using std::vector;
+
+std::string VERSION = "0.1.3";
 
 int main(int argc, char** argv)
 {
 	try
 	{
 		fgeal::initialize();
-		{
-			fgeal::Display display(640, 480, "Project Terrarium (v"+VERSION+") (fgeal v"+ fgeal::VERSION + "/" + fgeal::BACKEND_NAME +" backend)");
 
-	//		cout << "Lets test the directory listing method" << endl;
-	//		list<String> dirs = fgeal::getFilenamesWithinDirectory("./resources/maps");
-	//		for(list<String>::iterator it = dirs.begin(); it != dirs.end() ; ++it)
-	//			cout << *it << endl;
+		cout << "Lets test the directory listing method" << endl;
+		vector<string> dirs = fgeal::getFilenamesWithinDirectory("./resources/maps");
+		for(unsigned i = 0; i < dirs.size(); i++)
+			cout << dirs[i] << endl;
 
-			Image loading_image("./resources/loading.png");
-			loading_image.draw();
-			fgeal::display->refresh();
+		//only need to instantiate, engine will delete it when finalizing
+		new fgeal::Display(640, 480, "Project Terrarium (v"+VERSION+") (fgeal v"+ fgeal::VERSION + "/" + fgeal::BACKEND_NAME +" backend)");
 
-			MainMenu menu;
-			menu.show();
-		}
+		Image* loading_image = new Image("./resources/loading.png");
+		loading_image->draw();
+		fgeal::display->refresh();
+		delete loading_image;
+
+		TerrariumGame* game = new TerrariumGame();
+		game->start();
+		delete game;
 		fgeal::finalize();
 	}
 	catch(std::runtime_error& e)

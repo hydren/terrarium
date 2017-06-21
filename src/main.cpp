26d65b4454ece5425d3d1f76750168391c35e3d3
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2017-06-21 15:59:55 -0300

    Now game accepts cli parameters to set resolution.

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,16 @@
  *      Author: felipe
  */
 
-#include <cstdlib>
-#include <stdexcept>
+#include "terrarium_game.hpp"
 
 #include "fgeal/fgeal.hpp"
 using fgeal::Image;
 
-#include "terrarium_game.hpp"
+#include "futil/string/actions.hpp"
+#include "futil/string/split.hpp"
+
+#include <cstdlib>
+#include <stdexcept>
 
 #include <iostream>
 using std::cout;
@@ -23,36 +26,66 @@ using std::string;
 #include <vector>
 using std::vector;
 
-std::string VERSION = "0.1.6-dev";
+const string VERSION = "0.1.6-dev";
 
 int main(int argc, char** argv)
 {
+	string title = "Project Terrarium";
+	title += "(v"; title += VERSION; title += ")";
+
+	int screenWidth = 640, screenHeight = 480;
+	for(int i = 0; i < argc; i++)
+	{
+		if(starts_with(string(argv[i]), "-r"))
+		{
+			if(i+1 < argc)
+			{
+				string txtResolution(argv[i+1]);
+				vector<string> tokens = split(txtResolution, 'x');
+				if(tokens.size() == 2)
+				{
+					int customWidth =  atoi(tokens[0].c_str());
+					int customHeight = atoi(tokens[1].c_str());
+					if(customWidth != 0 and customHeight != 0)
+					{
+						screenWidth = customWidth;
+						screenHeight = customHeight;
+					}
+
+				}
+				else cout << "Failed to parse custom resolution" << endl;
+			}
+			else cout << "Missing argument to -r parameter" << endl;
+		}
+	}
+
 	try
 	{
 		TerrariumGame::CONFIG.load("config.properties");
 		fgeal::initialize();
 
-		cout << "Lets test the directory listing method" << endl;
-		vector<string> dirs = fgeal::getFilenamesWithinDirectory("./resources/maps");
-		for(unsigned i = 0; i < dirs.size(); i++)
-			cout << dirs[i] << endl;
+		// only need to instantiate, engine will delete it when finalizing
+		fgeal::Display::create(screenWidth, screenHeight, title);
 
-		//only need to instantiate, engine will delete it when finalizing
-		fgeal::Display::create(640, 480, "Project Terrarium (v"+VERSION+")");
-
+		// loading splash
 		Image* loading_image = new Image("./resources/loading.png");
 		loading_image->draw();
 		fgeal::Display::getInstance().refresh();
 		delete loading_image;
 
+		// start game
 		TerrariumGame* game = new TerrariumGame();
 		game->start();
 		delete game;
 		fgeal::finalize();
 	}
+	catch(const fgeal::AdapterException& e)
+	{
+		cout << "engine error " << e.what() << endl;
+	}
 	catch(std::runtime_error& e)
 	{
-		cout << e.what() << endl;
+		cout << "runtime error " << e.what() << endl;
 	}
 	return EXIT_SUCCESS;
 }

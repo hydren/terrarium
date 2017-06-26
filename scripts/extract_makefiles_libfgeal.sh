dd2fdc70a6aad6f1b1ab643c26aac01952b3ef04
Carlos Faruolo <5carlosfelipe5@gmail.com>
2017-06-26 00:29:41 -0300

    Renamed scripts.

--- scripts/extract_makefiles_libfgeal.sh
+++ scripts/extract_makefiles_libfgeal.sh
@@ -11,9 +11,9 @@ cd ..
 echo "- Copying makefiles from linux-gcc-libfgeal-release folder to build/others/linux-gcc-libfgeal-release folder..."
 cp linux-gcc-libfgeal-release/* -r build/others/linux-gcc-libfgeal-release/
 echo "- Replacing current folder with generic BUILD_PATH token..."
-./touch_makefiles.sh ./build/others/linux-gcc-libfgeal-release/ ${PWD} BUILD_PATH
+./scripts/touch_makefiles.sh ./build/others/linux-gcc-libfgeal-release/ ${PWD} BUILD_PATH
 echo "- Replacing current fgeal include folder with generic FGEAL_INCLUDE_PATH token..."
-./touch_makefiles.sh ./build/others/linux-gcc-libfgeal-release/ $1 FGEAL_INCLUDE_PATH
+./scripts/touch_makefiles.sh ./build/others/linux-gcc-libfgeal-release/ $1 FGEAL_INCLUDE_PATH
 echo "- Replacing current fgeal include folder with generic FGEAL_LIBS_PATH token..."
 find ./build/others/linux-gcc-libfgeal-release/ -name makefile -type f -exec echo "Patched " {} \;
 find ./build/others/linux-gcc-libfgeal-release/ -name makefile -type f -exec sed -i "s,$2,FGEAL_LIBS_PATH,g" {} \;

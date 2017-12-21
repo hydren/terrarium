6738e04d257109c7206b595a3f71876cad57fa16
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2017-12-21 17:21:22 -0200

    Created wiki branch

--- build/linux-gcc-allegro5.0-release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lBox2D -lallegro -lallegro_image -lallegro_font -lallegro_ttf -lallegro_primitives -lallegro_audio -lallegro_acodec
-
--- build/linux-gcc-sfml1.6-release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lBox2D -lsfml-system -lsfml-window -lsfml-graphics -lsfml-audio
-
